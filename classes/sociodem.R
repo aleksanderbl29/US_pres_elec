7c748c96481860f14eb1494f7f929a332f728b8c
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-09-25 18:40:36 +0200

    Add census

--- classes/sociodem.R
+++ classes/sociodem.R
@@ -1,4 +1,15 @@
-source("classes/exercise6.R")
+library(tidycensus)
+library(tidyverse)
 
-correlations
+census_key <- Sys.getenv("CENSUS_API_KEY")
+census_api_key(census_key)
 
+age20 <- get_decennial(geography = "state",
+                       variables = "P13_001N",
+                       year = 2020,
+                       sumfile = "dhc")
+
+deaths <- get_estimates(geography = "county",
+                     variables = "DEATHS")
+
+head(deaths)
